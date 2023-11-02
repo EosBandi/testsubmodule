42b2bb428cefb35735fedf3fcffcb8f4d802a31c
Andras Schaffer <eosbandi@gmail.com>
2023-11-02 23:05:44 +0100

    Create CommonUtils.cs
    Added a class

---
+++ CommonUtils.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOSBandi.Utilities
+{
+    public static class CommonUtils
+    {
+
+        public static void DoSomething(int i)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
